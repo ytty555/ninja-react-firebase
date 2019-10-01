4bd2a55435bedb1d737b1ff46ba900bb5e6552dd
Yuriy Titov <yurtitov@gmail.com>
2019-10-01 18:05:25 +0200

    some fixes

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -5,7 +5,6 @@ import { connect } from 'react-redux';
 
 class Dashboard extends Component {
   render() {
-    // console.log(this.props);
     const { projects } = this.props;
 
     return (
--- src/components/project/ProjectList.js
+++ src/components/project/ProjectList.js
@@ -2,17 +2,12 @@ import React from 'react';
 import ProjectSummary from './ProjectSummary';
 
 const ProjectList = ({ projects }) => {
-  const projectsList = projects.map(project => {
-    return (
-      <li key={project.id}>
-        <ProjectSummary project={project} />
-      </li>
-    );
-  });
-
   return (
     <div className='project-list section'>
-      <ul>{projectsList}</ul>
+      {projects &&
+        projects.map(project => {
+          return <ProjectSummary project={project} key={project.id} />;
+        })}
     </div>
   );
 };
